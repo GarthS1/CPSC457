4fe646278a493efb0ae3f587d5bcf2f47ff05798
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-06-05 16:43:54 -0600

    Update subset.cpp

--- subset.cpp
+++ subset.cpp
@@ -1,137 +1,180 @@
-// single threaded program that reads in 32-bit integers from
-// standard input, and then counds and prints out how many
-// subsets of these integers sum up to 0 (zero).
-//
-// this program uses a trivial exponential time algorithm, by
-// testing every possible subset (there are 2^N-1 subsets to
-// test for N integers)
-//
-// the program uses global variables - to make it a bit easier
-// to convert to a multi-threaded code for programmers
-// that have not used pthreads before
+/// reads in a positive integers in range (2 .. 2^63-1) from standard input
+/// for each number on input:
+///   if the number is prime, it is ignored
+///   determines the smallest factor of the composite number
+/// prints out the sum of all smallest factors
+///
+/// if no composite numbers are found, prints out "0"
+///
+/// compile with:
+///   $ g++ sumFactors.cpp -O3 -o gcd -lm
 
+// ON LINUX MACHINES REQUIRES COMPILING WITH -pthread 
 
-// For this assignment I converted the simple program into a multi threaded one 
-#include <stdio.h>
-#include <vector>
-#include <stdlib.h>
-#include <assert.h>
-#include <errno.h>
-#include <math.h>
-#include <thread>
-#include <unistd.h>
+// For this assignment made the single program into multithread, used barriers to reuse threads and ended the loop early if another thread
+// found a smaller prime. 
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cinttypes>
+#include <cmath>
 #include <iostream>
 #include <pthread.h>
 
-// global variables are bad ...
-// but they are acceptable for your first mulithreaded program
-std::vector<long> a;
-//removed count as a gloabal var as it wasn't needed
+int64_t smallPrime;  				// global variable to track smallest prime 
+pthread_mutex_t primeMutex;	// mutex used to make sure there are no race conditions for smallest prime
+pthread_mutex_t lockMutex;	// mutex used to make sure read is succesful
+int64_t sum;								// global sum of all the divisors 
+int64_t curPrime; 					// global variable to track which number was just read in 
+pthread_barrier_t myBarrier;// the barrier to reuse threads 
+bool readSuc;								// global varibable to check if read is succesful
+int64_t spiltSize;					// global variable for the spiltSize
+int nThreads;								// the amount of threads
+int64_t max;								// the current maximum of the prime numbers range 
 
-// for debugging purposes (if you want to print the combinations)
-void print_sol(long comb)
-{
-  return; // comment this out if you want to see the subsets
-  
-  for(size_t i = 0 ; i < a.size() ; i ++ ) {
-    if( comb & 1)
-      printf("%ld ", a[i]);
-    comb /= 2;
-  }
-  printf("\n");
+// read in the current prime number and return if read was succesful 
+bool read() {
+    if( ! (std::cin >> curPrime) )
+        return false;
+    else
+        return true; 
 }
 
-// test a combination 'comb'
-// the binary representation of comb will be used to decide
-// which numbers in a[] will be used to test the sum
-void test(long comb, int* count)
+// the inner function that caculates the range for the prime numbers 
+int64_t getSmallestDivisorInner(int64_t start, int64_t end)
 {
-  long long sum = 0;
-  long bits = comb;
-  for(size_t i = 0 ; i < a.size() ; i ++ ) {
-    if( bits & 1) // check lowest bit
-      sum += a[i];
-    bits >>= 1; // shift bits to the right
+	if( curPrime <= 3) return 0; // 2 and 3 are primes
+  if( curPrime % 2 == 0) return 2; // handle multiples of 2
+  if( curPrime % 3 == 0) return 3; // handle multiples of 3
+	
+	// loop to check all other possible primes end early if it is no longer possible to find a smaller prime 
+	while( start <= end && (smallPrime == 0 || start < smallPrime) ) 
+	{	
+    if (curPrime % start == 0) return start;
+    if (curPrime % (start+2) == 0) return start + 2;
+    start += 6;
   }
-  if( sum == 0) {
-    print_sol(comb);
-    (*count)++;  // defrence then increment 
-  }
+  return 0;
 }
 
-// test all combinatsion in range [from, to)
-void test_range( long from, long to, int* counter)
+// thread function which runs until all inputs are exhausted 
+void* getSmallestDivisor(void* n)
 {
-  for( long i = from ; i < to ; i ++)
-    test(i, counter);  //passes a counter to test
+	int threadId = (intptr_t) n;  //cast n into proper thread id 
+	while(1) 
+	{
+    // synchronize all threads
+    int res = pthread_barrier_wait(&myBarrier);
+		// do parallel work 
+    if( res == PTHREAD_BARRIER_SERIAL_THREAD)
+    {
+      pthread_mutex_lock(&lockMutex);
+      readSuc = read();  // read into global variable and check to make sure read was succesful
+			
+			max = sqrt(curPrime);
+			spiltSize = 6 * floor(max / nThreads /6);  //split in such a way that spiltsize is divisible by 6
+      pthread_mutex_unlock(&lockMutex);
+    }
+     
+    // synchronize again before starting parallel work
+    pthread_barrier_wait(&myBarrier);   
+
+    // run tasks in parallel
+    if (readSuc == true)        // if read was successful, run threads
+    {
+			int64_t temp;
+			if(curPrime < 500) // for small values no need to multithread create one thread and run it like the original code 
+			{
+				// synchronize all threads
+				int res = pthread_barrier_wait(&myBarrier);
+				// do parallel work 
+				if( res == PTHREAD_BARRIER_SERIAL_THREAD)
+				{
+					smallPrime = getSmallestDivisorInner(5, max);
+				}
+			}
+			else
+			{
+				if( (threadId + 1) * spiltSize < 5) // needed in case a vaule of less than 5 is meant to be the start point 
+					temp = getSmallestDivisorInner(5, max);
+				else if (threadId == nThreads - 1){
+					//needed in case it rounds in such a way it goes over or misses one 
+					temp = getSmallestDivisorInner(threadId * spiltSize + 5, max);
+				}
+				else  // run normally 
+					temp = getSmallestDivisorInner(threadId * spiltSize + 5, ((threadId + 1) * spiltSize) + 5);
+				
+				// for all threads check to see if this thread has the smallest prime 
+				pthread_mutex_lock(&primeMutex);
+				if( (smallPrime > temp || smallPrime == 0) && temp != 0)
+					smallPrime = temp;
+				pthread_mutex_unlock(&primeMutex);
+			}
+			
+			// synchronize all threads
+			int res = pthread_barrier_wait(&myBarrier);
+			// do parallel work
+			if( res == PTHREAD_BARRIER_SERIAL_THREAD)
+			{
+				//std::cout << "Debug: " << curPrime << " --> " << smallPrime << "\n";
+				sum += smallPrime;
+				smallPrime = 0;
+			}
+    }
+    else //if read was not succesful break loop
+    {
+			break;
+    }
+  }
+
+  pthread_exit(0);
+}
+
+// fucntion to make the proper threads and barrier
+void startThreads()
+{
+  sum = 0; 
+	
+	pthread_barrier_init(&myBarrier, NULL, nThreads);
+
+  // create threads
+  pthread_t threadPool[nThreads];
+  for (int i = 0; i < nThreads; i++)
+  {
+   pthread_create(&threadPool[i], NULL, getSmallestDivisor, (void *)i);
+  }
+
+  // when program is done finish the program 
+  for (int i = 0; i < nThreads; i++)
+  {
+    pthread_join(threadPool[i], NULL);
+  }
 }
 
 int main( int argc, char ** argv)
 {
-  //
-  // parse command line arguments
-  //
-  if( argc > 2) {
-    printf("Error: expected at most 1 command line argument.\n");
-    return -1;
+  /// parse command line arguments
+  nThreads = 1;
+  if( argc != 1 && argc != 2) {
+    std::cout << "Usage: " << argv[0] << " [nThreads]\n";
+    exit(-1);
   }
-  long nThreads = 1;
-  if( argc > 1) {
-    // convert argv[1] to 'nThreads', report error if failed
-    char * end = 0;
-    errno = 0;
-    nThreads = strtol( argv[1], & end, 10);
-    if( * end != 0 || errno != 0) {
-      printf("Expected integer, but you gave me '%s'\n", argv[1]);
-      return -1;
-    }
+  if( argc == 2) nThreads = atoi( argv[1]);
+
+  /// handle invalid arguments
+  if( nThreads < 1 || nThreads > 256) {
+    std::cout << "Warning: Bad arguments. 1 <= nThreads <= 256!\n";
   }
 
-  //
-  // read all integers one by one and put them into the array
-  //
-  while(1) {
-    long n;
-    if( 1 != scanf("%ld", & n)) break;
-    a.push_back(n);
-  }
+  std::cout << "Using " << nThreads
+            << " thread" << (nThreads == 1 ? "" : "s")
+            << ".\n";
 
-  // debug message
-  printf( "Using %ld thread(s) on %lu numbers.\n",
-	  nThreads, a.size());
-	
-	//need this to avoid making threads that do no work 
-	if(nThreads > a.size())
-		nThreads = a.size();
-	
-	//spilts into 2^n / m where n = #numbers and m = #threads and then uses celling to force an integer
-  long spiltSize = ceill((long(1) << a.size()) / nThreads); 
-	
-	int counters[nThreads];  //an array of count to avoid race conditions 
-	
-	//set all to 0
-	for(int i = 0; i < nThreads; i++)
-		counters[i] = 0;
-	
-	std::thread threadPool[nThreads];  //a thread pool used to run all the threads needed 
-	
-	//run all the threads needed 
-	for (int i = 0; i < nThreads; i++)
-		if (i == nThreads - 1) //needed in case it rounds in such a way it goes over or misses one 
-			threadPool[i] = std::thread(test_range, i * spiltSize + 1,long(1) << a.size(), &counters[i]);
-		else	
-			threadPool[i] = std::thread(test_range, i * spiltSize + 1,(i + 1) * spiltSize, &counters[i]);
+	// call the function to run the threads 
+	startThreads();
 
-  //wait for every thread to finish
-  for (int i = 0; i < nThreads; i++)    
-		threadPool[i].join();    
-
-	//add up all the counters to find the total count 
-  long count = 0;
-	for (int i = 0; i < nThreads; i++)
-	  count += counters[i];
-
-  printf("Subsets found: %ld\n", count);
+  /// report results
+  std::cout << "Sum of divisors = " << sum << "\n";
 
   return 0;
 }
